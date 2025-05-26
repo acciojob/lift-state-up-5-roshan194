477a8df7ec9143d32c526c3599ce40d7e905199a
Roshan Kumar <roshan@Roshans-MacBook-Air.local>
2025-05-26 10:14:53 +0530

    add path

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LoginForm from "./LoginForm";
 import './../styles/App.css';
 
 const App = () => {
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
-import LoginForm from "./LoginForm";
+
 function LoginForm({ isLoggedIn, onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Simulate login (no validation here for simplicity)
     if (username && password) {
-      onLogin(); // Lifts the state change to the parent
+      onLogin();
     }
   };
 
